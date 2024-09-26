bc77df6cbe5136bf65aaae6925f0fe0e51bb2fce
Karl Felix Schewe <info@avoirli.eu>
2024-09-26 16:14:15 +0200

    Fix neovim config dir

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,4 +2,7 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
-vim.opt.clipboard = "unnamedplus"
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+require("config.lazy")
