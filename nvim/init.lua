d3cb1c140e6a059cd82ae8fa604bcca0fe71b773
Karl Felix Schewe <info@avoirli.eu>
2024-10-16 22:21:35 +0200

    Switch to LazyVim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,2 @@
--- Tab replacement
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
--- Line numbers
-vim.opt.number = true
-vim.opt.relativenumber = true
-
--- Plugins
+-- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
